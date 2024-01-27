ddd09f8eed9afae975db5a11d2d347a6ee85debb
pragatiray <93862662+pragatiray@users.noreply.github.com>
2024-01-27 06:46:51 +0000

    added some scientific function

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    
+document.addEventListener("DOMContentLoaded", function ()
+ {
+
     const display = document.getElementById('calc-display');
     const buttons = document.getElementsByClassName('btn');
 
@@ -8,6 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
     function evaluateResult() 
     {
         console.log('currentValue:',currentValue);
+        const convertedValue = currentValue
+        .replace("×","*")
+        .replace("÷","/")
+        .replace('%','*0.01')
+        .replace("sin",'Math.sin')
+        .replace('cos','Math.cos')
+        .replace('ln','Math.log')
+        .replace('π','Math.PI')
+        .replace('log','Math.log10')
+        .replace('e','Math.E')
+        .replace('tan','Math.tan')
+        .replace('√','Math.sqrt');
+
+
+        console.log('convertedValue',convertedValue)
         const result = eval(currentValue);
         currentValue = result.toString();
         display.value = currentValue;
@@ -20,17 +36,26 @@ document.addEventListener("DOMContentLoaded", function () {
         {
         const value = button.innerText;
 
-        if (value == "AC") {
-            currentValue = "";
-            display.value = currentValue;
-        }
-        else if (value == "=") {
-            evaluateResult();
-        }
-        else{
-            currentValue += value;
-            display.value = currentValue;
-        }
+            try
+            {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                }
+                else if (value == "=") {
+                    evaluateResult();
+                }
+                else{
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            }
+            catch(error)
+            {
+                console.error(error);
+                currentValue=error;
+                display.value=currentValue;
+            }
            
         })
     }
